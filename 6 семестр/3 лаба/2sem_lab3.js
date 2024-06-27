799a894fa720ec6da865aeeff7e0084ff2997c96
Alexandra <118150358+kssaxa@users.noreply.github.com>
2024-06-27 16:42:46 +1000

    Update 2sem_lab3.js

--- 6 семестр/3 лаба/2sem_lab3.js
+++ 6 семестр/3 лаба/2sem_lab3.js
@@ -1,65 +1,126 @@
-let displayTable = (data, idTable) => {
-    let table = document.getElementById(idTable).getElementsByTagName('tbody')[0];
-    table.innerHTML = ""; // Clear existing table data
+let createTable = (data, idTable) => {
+    let table = document.getElementById(idTable);
+    clearTable(idTable); // Очищаем таблицу перед заполнением
 
+    if (data.length === 0) {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.colSpan = Object.keys(data[0]).length;
+        td.innerHTML = "No results found";
+        tr.append(td);
+        table.append(tr);
+        return;
+    }
 
     let tr = document.createElement('tr');
-    for (key in data[0]) {
-        // формируем заголовочную строку из ключей нулевого элемента массива
+    for (let key in data[0]) {
         let th = document.createElement('th');
         th.innerHTML = key;
         tr.append(th);
     }
     table.append(tr);
-
-    // Populate table with data
     data.forEach((item) => {
         tr = document.createElement("tr");
-        for (value in item) {
+        for (let value in item) {
             let td = document.createElement("td");
             td.innerHTML = item[value];
             tr.append(td);
         }
         table.append(tr);
     });
-    // создать новую строку таблицы tr
-    // перебрать ключи очередного элемента массива
-    // создать элемент td
-    // занести в него соответствующее значение из массива
-    // добавить элемент td к строке
-    // строку добавить в таблицу
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    createTable(agencies, 'list');
 
-function filterTable() {
-    let name = document.getElementById('Name').value.toLowerCase();
-    let region = document.getElementById('Region').value.toLowerCase();
-    let deals = document.getElementById('Deals').value;
-    let ads = document.getElementById('Ads').value;
-    let ratingFrom = parseFloat(document.getElementById('RatingFrom').value);
-    let ratingTo = parseFloat(document.getElementById('RatingTo').value);
-
-    let filteredData = agencies.filter(agency => {
-        return (
-            (name === '' || agency['Агенство'].toLowerCase().includes(name)) &&
-            (region === '' || agency['Субъект федерации'].toLowerCase().includes(region)) &&
-            (deals === '' || agency['Количество сделок'] == deals) &&
-            (ads === '' || agency['Количество объявлений'] == ads) &&
-            (isNaN(ratingFrom) || agency['Оценка клиентов'] >= ratingFrom) &&
-            (isNaN(ratingTo) || agency['Оценка клиентов'] <= ratingTo)
-        );
+    document.getElementById("search").addEventListener("click", function () {
+        let dataForm = document.getElementById("filter");
+        filterTable(agencies, "list", dataForm);
     });
 
-    displayTable(filteredData, 'list');
-}
+    document.getElementById("clear").addEventListener("click", function () {
+        clearFilter();
+    });
 
+    document.getElementById("buttonSort").addEventListener("click", function () {
+        sortTable();
+    });
 
-function clearFilter() {
-    document.getElementById('filter').reset();
-    displayTable(agencies, 'list');
-}
+    document.getElementById("buttonClear").addEventListener("click", function() {
+        resetSort();
+    });
+});
 
+let correspond = {
+    'Агенство': 'Name',
+    'Субъект федерации': 'Region',
+    'Количество сделок': 'Deals',
+    'Количество объявлений': 'Ads',
+    'Оценка клиентов': ['RatingFrom', 'RatingTo']
+};
 
+let dataFilter = (dataForm) => {
+    let dictFilter = {};
+    for (let j = 0; j < dataForm.elements.length; j++) {
+        let item = dataForm.elements[j];
+        let valInput = item.value;
+        if (item.type === "text") {
+            valInput = valInput.toLowerCase();
+        } else if (item.type === "number") {
+            if (valInput === "") {
+                if (item.id.includes("RatingFrom")) {
+                    valInput = Number.NEGATIVE_INFINITY;
+                } else if (item.id.includes("RatingTo")) {
+                    valInput = Number.POSITIVE_INFINITY;
+                }
+            } else {
+                valInput = Number(valInput);
+            }
+        }
+        dictFilter[item.id] = valInput;
+    }
+    return dictFilter;
+};
+
+let filterTable = (data, idTable, dataForm) => {
+    let datafilter = dataFilter(dataForm);
+
+    let tableFilter = data.filter(item => {
+        let result = true;
+        for (let key in item) {
+            let val = item[key];
+            if (typeof val === 'string') {
+                val = val.toLowerCase();
+                if (datafilter[correspond[key]]) {
+                    result &&= val.includes(datafilter[correspond[key]]);
+                }
+            } else if (typeof val === "number") {
+                if (typeof correspond[key] === "object") {
+                    result &&= datafilter[correspond[key][0]] <= val && val <= datafilter[correspond[key][1]];
+                } else {
+                    result &&= datafilter[correspond[key]] === '' ? true : val === Number(datafilter[correspond[key]]);
+                }
+            }
+        }
+        return result;
+    });
+
+    clearTable(idTable);
+    createTable(tableFilter, idTable);
+};
+
+function clearTable(idTable) {
+    let table = document.getElementById(idTable);
+    while (table.rows.length > 0) {
+        table.deleteRow(0);
+    }
+};
+
+let clearFilter = () => {
+    document.getElementById("filter").reset();
+    clearTable("list");
+    createTable(agencies, "list");
+};
 function sortTable() {
     let level1 = document.getElementById('sortLevel1').value;
     let order1 = document.getElementById('orderLevel1').value;
@@ -72,9 +133,8 @@ function sortTable() {
         return multiLevelSort(a, b, [[level1, order1], [level2, order2], [level3, order3]]);
     });
 
-    displayTable(sortedData, 'list');
+    createTable(sortedData, 'list');
 }
-
 
 function multiLevelSort(a, b, levels) {
     let keys = ["Агенство", "Субъект федерации", "Количество сделок", "Количество объявлений", "Оценка клиентов"];
@@ -85,30 +145,7 @@ function multiLevelSort(a, b, levels) {
     }
     return 0;
 }
-
-
 function resetSort() {
     document.getElementById('sortForm').reset();
-    displayTable(agencies, 'list');
+    createTable(agencies, 'list');
 }
-
-// Event listeners
-document.addEventListener("DOMContentLoaded", function() {
-    displayTable(agencies, 'list');
-
-    document.getElementById("search").addEventListener("click", function () {
-        filterTable();
-    });
-
-    document.getElementById("clear").addEventListener("click", function() {
-        clearFilter();
-    });
-
-    document.getElementById("buttonSort").addEventListener("click", function () {
-        sortTable();
-    });
-
-    document.getElementById("buttonClear").addEventListener("click", function() {
-        resetSort();
-    });
-});
